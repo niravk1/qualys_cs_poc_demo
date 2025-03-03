158ce79f055b9265161b6b293a7d6722c6d9c6d7
Nirav Kamdar <nkamdar@qualys.com>
2025-03-03 20:12:59 +1100

    Update main.tf

--- aws/cs_eks/main.tf
+++ aws/cs_eks/main.tf
@@ -56,6 +56,7 @@ module "vpc" {
 }
 
 resource "aws_security_group" "node_group_one" {
+  iac : exempt = 289 : Skipping cid 289
   name_prefix = "node_group_one"
   vpc_id      = module.vpc.vpc_id
 
